508c9dcf1bd2e56ff77dd3aa565ad630b7698741
unknown <sarthakmjs213@gmail.com>
2024-04-05 17:41:34 +0530

    Display added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import Clock from "./Clock"
-import ToDoList from "./ToDoList"
-import Timer from "./timer"
+// App.js
+
+import React from 'react';
+import './App.css';
+import Clock from './Clock.jsx';
+import Timer from './timer.jsx';
+import ToDoList from './ToDoList.jsx';
 
 function App() {
   return (
-    <>
-    
-    
-    <Timer/>
-    </>
-  )
+    <div className="app">
+      <div className="left-column">
+        <Clock/>
+        <Timer/>
+      </div>
+      <div className="right-column">
+        <ToDoList/>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
